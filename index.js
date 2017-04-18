4b81fad1df18c0ad6f679497a2f27ff26ba4c50e
Matthew A. Obrien <M8HY6@regions.com>
2017-04-18 15:54:19 -0500

    Trying a different version.

--- index.js
+++ index.js
@@ -8,130 +8,19 @@ const CreateCatalog=`CREATE TABLE Catalog (
     CatalogID varchar(10) not null primary key
 );`;
 
-const CreateCatalogName=`CREATE TABLE CatalogName (
-     CatalogID varchar(10) not null,
-     Name varchar(50) not null, 
-     DateModified datetime not null, 
-     Constraint pkCatalogName PRIMARY KEY(CatalogID, Name), 
-     Constraint fkCatalogNameToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID)
-);`;
-
-const CreateCatalogDeterminationLookup=`CREATE TABLE CatalogDeterminationLookup (
-    DeterminationType varchar(30) not null primary key
-);`
-const CreateCatalogDetermination=`CREATE TABLE CatalogDetermination (
-    CatalogID varchar(10) not null,
-    DeterminationType varchar(10) not null,
-    DateModified datetime not null,
-    CONSTRAINT pkCatalogDetermination PRIMARY KEY(CatalogID, DeterminationType, DateModified),
-    CONSTRAINT fkCatalogDeterminationToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID),
-    CONSTRAINT fkCatalogDeterminationToLookup FOREIGN KEY(DeterminationType) REFERENCES CatalogDeterminationLookup(DeterminationType)
-)
-`
-
-const CreateCatalogRatingLookup=`CREATE TABLE CatalogRatingLookup (
-    Rating varchar(30) not null primary key
-);`
-const CreateCatalogRating=`CREATE TABLE CatalogRating (
-    CatalogID varchar(10) not null,
-    Rating varchar(10) not null,
-    DateModified datetime not null,
-    CONSTRAINT pkCatalogRating PRIMARY KEY(CatalogID, Rating, DateModified),
-    CONSTRAINT fkCatalogRatingToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID),
-    CONSTRAINT fkCatalogRatingToLookup FOREIGN KEY(Rating) REFERENCES CatalogRatingLookup(Rating)
-)
-`
+const CreateLongForm=`CREATE TABLE LongForm (
+    ActivityID varchar(10) not null primary key,
+    ActivityName char(100) not null,
+    ActivityType char(50) not null,
+    MVLead char(50) not null,
+    ActivityDate datetime not null,
+    MRMVVersion varchar(20) not null, 
+    ModelClass char(10) not null,
+    ERABusinessGroup varchar(20) not null,
 
 
-/**Annual review, validation, significant change, 
- * OGM, determinations */
-const CreateActivitiesLookup=`CREATE TABLE ActivityLookup(
-    ActivityType varchar(50) not null primary key
-);`;
-
-const CreateActivityID=`
-CREATE TABLE Activity(
-    ActivityID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ActivityType varchar(50) not null,
-    CatalogID,
-    CONSTRAINT fkActivityType FOREIGN KEY(ActivityType) REFERENCES ActivityLookup(ActivityType)
-);
-`
-
-const ActivityTypes=[
-    "Validation", 
-    "AnnualReview",
-    "Exam",
-    "Determination",
-    "SignificantChange",
-    "OGM",
-    "IssueRemediation"
-]
-
-
-/**What is an action? */
-
-/**Each activity will have its own table (actions) so that 
- * each activity will have its own set of actions
- */
-const CreateActivityQueries=ActivityTypes.map((val)=>{
-    return {
-        lookup:`
-            CREATE TABLE ${val}ActionLookup(
-                Action varchar(30) not null primary key
-            );
-            `,
-        mainQuery:`
-            CREATE TABLE ${val}Action(
-                ActivityID INTEGER NOt null,
-                Action varchar(30) not null,
-                DateModified datetime not null,
-                CONSTRAINT pk${val}Action PRIMARY KEY(ActivityID, Action),
-                CONSTRAINT fk${val}Action FOREIGN KEY(Action) REFERENCES
-                    ${val}ActionLookup(Action)
-            );
-        `
-    }
-})
-
-
-/*Issues severity lookup*/
-const CreateIssueSeverityLookupTable=`
-CREATE TABLE IssueSeverityLookup(
-    IssueSeverity varchar(30) not null primary key
-);`
-
-/* Issues table*/
-const CreateIssuesTable=`
-CREATE TABLE Issues(
-    IssueID Integer not null primary key,
-    ActivityID INTEGER not null,
-    CONSTRAINT fkIssueActivity FOREIGN KEY(ActivityID) REFERENCES Activity(ActivityID)
-);`
-
-/**issues severity table (for downgrades/upgrades) */
-const CreateIssueSeverity=`
-CREATE TABLE IssueSeverity(
-    IssueID Integer not null,
-    IssueSeverity varchar(30) not null,
-    DateModified datetime not null,
-    CONSTRAINT pkIssueSeverity PRIMARY KEY(IssueID, IssueSeverity, DateModified),
-    CONSTRAINT fkIssueSeverityToIssueID FOREIGN KEY(IssueID) REFERENCES Issues(IssueID),
-    CONSTRAINT fkISsueSeverirtyToLookup FOREIGN KEY(IssueSeverity) REFERENCES IssueSeverityLookup(IssueSeverity)
-)
-`
-
-/**Issue mapping table (for recasts) */
-const CreateIssueMapping=`
-CREATE TABLE IssueMapping(
-    PriorIssueID Integer not null, 
-    RecastIssueID INteger not null,
-    Constraint pkIssueMapping PRIMARY KEY(PriorIssueID, RecastIssueID),
-    Constraint fkPriorIssue FOREIGN KEY(PriorIssueID) REFERENCES Issues(ISsueID),
-    Constraint fkRecastIssue FOREIGN KEY(RecastIssueID) REFERENCES Issues(ISsueID)
-)
-`
-
+    STOPPED HERE.
+    `
 
 
 //db.all(`SELECT id, skill from AssociateSkills;`, cb)
