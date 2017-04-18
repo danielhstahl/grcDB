f9332c7f3a460136a6416d325c50be3aba9ed754
Daniel Stahl <B0GX3@regions.com>
2017-04-18 09:50:44 -0500

    inital tests and schema

--- index.js
+++ index.js
@@ -1,6 +1,136 @@
 'use strict';
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database(':memory:');
+db.exec("PRAGMA foreign_keys = ON;")
+
+/*This will include all items (models, tools, etc) including procedures*/
+const CreateCatalog=`CREATE TABLE Catalog (
+    CatalogID varchar(10) not null primary key
+);`;
+
+const CreateCatalogName=`CREATE TABLE CatalogName (
+     CatalogID varchar(10) not null,
+     Name varchar(50) not null, 
+     DateModified datetime not null, 
+     Constraint pkCatalogName PRIMARY KEY(CatalogID, Name), 
+     Constraint fkCatalogNameToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID)
+);`;
+
+const CreateCatalogDeterminationLookup=`CREATE TABLE CatalogDeterminationLookup (
+    DeterminationType varchar(30) not null primary key
+);`
+const CreateCatalogDetermination=`CREATE TABLE CatalogDetermination (
+    CatalogID varchar(10) not null,
+    DeterminationType varchar(10) not null,
+    DateModified datetime not null,
+    CONSTRAINT pkCatalogDetermination PRIMARY KEY(CatalogID, DeterminationType, DateModified),
+    CONSTRAINT fkCatalogDeterminationToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID),
+    CONSTRAINT fkCatalogDeterminationToLookup FOREIGN KEY(DeterminationType) REFERENCES CatalogDeterminationLookup(DeterminationType)
+)
+`
+
+const CreateCatalogRatingLookup=`CREATE TABLE CatalogRatingLookup (
+    Rating varchar(30) not null primary key
+);`
+const CreateCatalogRating=`CREATE TABLE CatalogRating (
+    CatalogID varchar(10) not null,
+    Rating varchar(10) not null,
+    DateModified datetime not null,
+    CONSTRAINT pkCatalogRating PRIMARY KEY(CatalogID, Rating, DateModified),
+    CONSTRAINT fkCatalogRatingToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID),
+    CONSTRAINT fkCatalogRatingToLookup FOREIGN KEY(Rating) REFERENCES CatalogRatingLookup(Rating)
+)
+`
+
+
+/**Annual review, validation, significant change, 
+ * OGM, determinations */
+const CreateActivitiesLookup=`CREATE TABLE ActivityLookup(
+    ActivityType varchar(50) not null primary key
+);`;
+
+const CreateActivityID=`
+CREATE TABLE Activity(
+    ActivityID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ActivityType varchar(50) not null,
+    CatalogID,
+    CONSTRAINT fkActivityType FOREIGN KEY(ActivityType) REFERENCES ActivityLookup(ActivityType)
+);
+`
+
+const ActivityTypes=[
+    "Validation", 
+    "AnnualReview",
+    "Exam",
+    "Determination",
+    "SignificantChange",
+    "OGM",
+    "IssueRemediation"
+]
+
+
+/**What is an action? */
+
+/**Each activity will have its own table (actions) so that 
+ * each activity will have its own set of actions
+ */
+const CreateActivityQueries=ActivityTypes.map((val)=>{
+    return {
+        lookup:`
+            CREATE TABLE ${val}ActionLookup(
+                Action varchar(30) not null primary key
+            );
+            `,
+        mainQuery:`
+            CREATE TABLE ${val}Action(
+                ActivityID INTEGER NOt null,
+                Action varchar(30) not null,
+                DateModified datetime not null,
+                CONSTRAINT pk${val}Action PRIMARY KEY(ActivityID, Action),
+                CONSTRAINT fk${val}Action FOREIGN KEY(Action) REFERENCES
+                    ${val}ActionLookup(Action)
+            );
+        `
+    }
+})
+
+
+/*Issues severity lookup*/
+const CreateIssueSeverityLookupTable=`
+CREATE TABLE IssueSeverityLookup(
+    IssueSeverity varchar(30) not null primary key
+);`
+
+/* Issues table*/
+const CreateIssuesTable=`
+CREATE TABLE Issues(
+    IssueID Integer not null primary key,
+    ActivityID INTEGER not null,
+    CONSTRAINT fkIssueActivity FOREIGN KEY(ActivityID) REFERENCES Activity(ActivityID)
+);`
+
+/**issues severity table (for downgrades/upgrades) */
+const CreateIssueSeverity=`
+CREATE TABLE IssueSeverity(
+    IssueID Integer not null,
+    IssueSeverity varchar(30) not null,
+    DateModified datetime not null,
+    CONSTRAINT pkIssueSeverity PRIMARY KEY(IssueID, IssueSeverity, DateModified),
+    CONSTRAINT fkIssueSeverityToIssueID FOREIGN KEY(IssueID) REFERENCES Issues(IssueID),
+    CONSTRAINT fkISsueSeverirtyToLookup FOREIGN KEY(IssueSeverity) REFERENCES IssueSeverityLookup(IssueSeverity)
+)
+`
+
+/**Issue mapping table (for recasts) */
+const CreateIssueMapping=`
+CREATE TABLE IssueMapping(
+    PriorIssueID Integer not null, 
+    RecastIssueID INteger not null,
+    Constraint pkIssueMapping PRIMARY KEY(PriorIssueID, RecastIssueID),
+    Constraint fkPriorIssue FOREIGN KEY(PriorIssueID) REFERENCES Issues(ISsueID),
+    Constraint fkRecastIssue FOREIGN KEY(RecastIssueID) REFERENCES Issues(ISsueID)
+)
+`
 
 
 
@@ -11,7 +141,24 @@ let db = new sqlite3.Database(':memory:');
 
 
 db.serialize(()=>{
-    //db.exec(query, cb)
+    db.exec(CreateCatalog)
+    db.exec(CreateCatalogName)
+    db.exec(CreateCatalogDeterminationLookup)
+    db.exec(CreateCatalogDetermination)
+    db.exec(CreateCatalogRatingLookup)
+    db.exec(CreateCatalogRating)
+    db.exec(CreateActivitiesLookup)
+    db.exec(CreateActivityID)
+    CreateActivityQueries.map((val)=>{
+        db.exec(val.lookup)
+        db.exec(val.mainQuery)
+    })
+    
+    db.exec(CreateIssueSeverityLookupTable)
+    db.exec(CreateIssuesTable)
+    db.exec(CreateIssueSeverity)
+    db.exec(CreateIssueMapping)
 });
 
 module.exports.db=db
+
--- index.test.js
+++ index.test.js
@@ -1,4 +1,38 @@
-const db=require('./index')
-it("Fails insert because of PK", (done)=>{
-    done()
+const data=require('./index')
+const db=data.db
+beforeAll(()=>{
+    db.serialize(()=>{
+        db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat1');")
+        db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat2');")
+        db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat3');")
+    })
+    
 })
+
+it("Fails insert because of Catalog PK", (done)=>{
+    db.exec(`INSERT INTO CatalogName (CatalogID, Name, DateModified) 
+        VALUES (
+            'cat5', 'My cat1 name', datetime('now')
+        )`, (err)=>{
+            if(err){
+                return done()
+            }
+            throw new Error("Should have errored")
+            done()
+        })
+})
+it("inserts into CatalogName", (done)=>{
+    db.exec(`INSERT INTO CatalogName (CatalogID, Name, DateModified) 
+        VALUES (
+            'cat1', 'My cat1 name', datetime('now')
+        )`, (err, result)=>{
+            if(err){
+                throw new Error("Should have worked")
+            }
+            return done()
+        })
+})
+
+
+
+
