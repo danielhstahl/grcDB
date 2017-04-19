55b3cbee91e2ada163774e0e081f165c2fd5b0c4
Daniel Stahl <B0GX3@regions.com>
2017-04-19 08:51:17 -0500

    improved tests and added to schema

--- index.js
+++ index.js
@@ -8,6 +8,47 @@ const CreateCatalog=`CREATE TABLE Catalog (
     CatalogID varchar(10) not null primary key
 );`;
 
+/**catalog events include development date, implementation date, retirement date.  
+ * "status" will be configured by these dates.  eg, "active" will be where implementation date exists but retirement doesn't
+  */
+const CreateCatalogEventLookup=`
+CREATE TABLE CatalogEventLookup (
+    CatalogEvent varchar(30) not null primary key    
+);`
+/** */
+const CreateCatalogEvent=`
+CREATE TABLE CatalogEvents (
+    CatalogID varchar(10) not null,
+    DateOfEvent date not null,
+    DateModified datetime not null,
+    CatalogEvent varchar(30) not null,
+    constraint pkCatalogEvents PRIMARY KEY(CatalogId, DateOfEvent, CatalogEvent),
+    Constraint fkCatalogEventsToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID),
+    Constraint fkCatalogEventsToCatalogLookup FOREIGN KEY(CatalogEvent) REFERENCES CatalogEventLookup(CatalogEvent)
+);
+`
+/**the "owner" field will be verified against AD.  
+ * All other fields (eg, business group, etc) will be populated by AD  */
+const CreateCatalogOwner=`
+CREATE TABLE CatalogOwner (
+    CatalogId varchar(10) not null,
+    DateModified datetime not null,
+    Owner varchar(10) not null,
+    Constraint pkCatalogOwner PRIMARY KEY(CatalogID, DateModified, Owner),
+    Constraint fkCatalogOwnerToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID)
+)
+`
+
+/**track upstream and downstream models */
+const CreateCatalogPrecidence=`
+CREATE TABLE CatalogDependencies (
+    UpstreamCatalogId varchar(10) not null,
+    DownstreamCatalogId varchar(10) not null,
+    CONSTRAINT pkCatalogDependencies PRIMARY KEY(UpstreamCatalogId, DownstreamCatalogId),
+    CONSTRAINT fkUpstream FOREIGN KEY(UpstreamCatalogId) REFERENCES Catalog(CatalogID),
+    CONSTRAINT fkDownstream FOREIGN KEY(DownstreamCatalogId) REFERENCES Catalog(CatalogID),
+);`;
+
 const CreateCatalogName=`CREATE TABLE CatalogName (
      CatalogID varchar(10) not null,
      Name varchar(50) not null, 
@@ -41,7 +82,29 @@ const CreateCatalogRating=`CREATE TABLE CatalogRating (
     CONSTRAINT fkCatalogRatingToLookup FOREIGN KEY(Rating) REFERENCES CatalogRatingLookup(Rating)
 )
 `
-
+/**analyst and lead will be verified against AD.  
+ * Lead can be automatically entered by using the 
+ * id of the person submitting the form */
+const CreateCatalogMRMV=`CREATE TABLE CatalogAssignment (
+    CatalogID varchar(10) not null,
+    Analyst varchar(10) not null,
+    Lead varchar(10) not null,
+    DateModified datetime not null,
+    CONSTRAINT pkCatalogAssignment PRIMARY KEY(CatalogID, Analyst, Lead, DateModified),
+    CONSTRAINT fkAssignmentToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID)
+)`
+/**Use is the description of what the model is used for.
+ * Use that is no longer applicable 
+ * has a new entry with Applicable=false.
+ */
+const CreateCatalogUses=`CREATE TABLE CatalogUses (
+    CatalogID varchar(10) not null,
+    Use varchar(50) not null,
+    DateModified datetime not null,
+    Applicable boolean not null,
+    CONSTRAINT pkCatalogUses PRIMARY KEY(CatalogID, Use, Applicable, DateModified),
+    CONSTRAINT fkCatalogUsesToCatalog FOREIGN KEY(CatalogID) REFERENCES Catalog(CatalogID)
+);`
 
 /**Annual review, validation, significant change, 
  * OGM, determinations */
@@ -69,7 +132,10 @@ const ActivityTypes=[
 ]
 
 
-/**What is an action? */
+/**What is an action? 
+ * I think an action is like "start", "draft published"
+ * ...should it include QC checkpoints?
+ * should it include items required for a validation like "scoping", "skills assessment"?*/
 
 /**Each activity will have its own table (actions) so that 
  * each activity will have its own set of actions
@@ -93,6 +159,8 @@ const CreateActivityQueries=ActivityTypes.map((val)=>{
         `
     }
 })
+
+
 
 
 /*Issues severity lookup*/
@@ -132,17 +200,15 @@ CREATE TABLE IssueMapping(
 )
 `
 
-
-
-//db.all(`SELECT id, skill from AssociateSkills;`, cb)
-//db.run(`INSERT INTO GroupKey (AdGroup, Key, dateCreated) VALUES (?, ?, datetime('now'))`, [group, key], cb);
-//db.exec(query, cb)
-
-
-
 db.serialize(()=>{
     db.exec(CreateCatalog)
     db.exec(CreateCatalogName)
+    db.exec(CreateCatalogEventLookup)
+    db.exec(CreateCatalogEvent)
+    db.exec(CreateCatalogPrecidence)
+    db.exec(CreateCatalogOwner)
+    db.exec(CreateCatalogUses)
+    db.exec(CreateCatalogMRMV)
     db.exec(CreateCatalogDeterminationLookup)
     db.exec(CreateCatalogDetermination)
     db.exec(CreateCatalogRatingLookup)
--- index.test.js
+++ index.test.js
@@ -1,19 +1,46 @@
 const data=require('./index')
 const db=data.db
-beforeAll(()=>{
-    db.serialize(()=>{
-        db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat1');")
-        db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat2');")
-        db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat3');")
-    })
+beforeEach(()=>{
+    //db.serialize(()=>{
+    db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat1');")
+    db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat2');")
+    db.exec("INSERT INTO Catalog (CatalogID) VALUES ('cat3');")
+    db.exec("INSERT INTO CatalogEventLookup (CatalogEvent) VALUES ('Development Date');")
+    db.exec("INSERT INTO CatalogEventLookup (CatalogEvent) VALUES ('Implementation Date');")
+    db.exec("INSERT INTO CatalogEventLookup (CatalogEvent) VALUES ('Retirement Date');")
+    db.exec(`INSERT INTO CatalogDeterminationLookup (DeterminationType) VALUES ('Model');`)
+    db.exec(`INSERT INTO CatalogDeterminationLookup (DeterminationType) VALUES ('Tool');`)
+    db.exec(`INSERT INTO CatalogDeterminationLookup (DeterminationType) VALUES ('Procedures');`)
+    db.exec(`INSERT INTO CatalogRatingLookup (Rating) VALUES ('Pass');`)
+    db.exec(`INSERT INTO CatalogRatingLookup (Rating) VALUES ('Low-Pass');`)
+    db.exec(`INSERT INTO CatalogRatingLookup (Rating) VALUES ('Fail');`)
+    db.exec(`INSERT INTO ActivityLookup (ActivityType) VALUES ('Validation');`)
+    db.exec(`INSERT INTO ActivityLookup (ActivityType) VALUES ('Annual Review');`)
+    db.exec(`INSERT INTO ActivityLookup (ActivityType) VALUES ('Ongoing Monitoring');`)
+    db.exec(`INSERT INTO IssueSeverityLookup (IssueSeverity) VALUES ('High');`)
+    db.exec(`INSERT INTO IssueSeverityLookup (IssueSeverity) VALUES ('Moderate');`)
+    db.exec(`INSERT INTO IssueSeverityLookup (IssueSeverity) VALUES ('Low');`)
+    db.exec(`INSERT INTO IssueSeverityLookup (IssueSeverity) VALUES ('Extreme');`)
+
+    //})
     
 })
 
-it("Fails insert because of Catalog PK", (done)=>{
+afterEach(()=>{
+    db.exec("DELETE FROM Catalog;")
+    db.exec("DELETE FROM CatalogDeterminationLookup;")
+    db.exec("DELETE FROM CatalogEventLookup;")
+    db.exec("DELETE FROM CatalogRatingLookup;")
+    db.exec("DELETE FROM ActivityLookup;")
+    db.exec("DELETE FROM IssueSeverityLookup;")
+})
+
+it("Fails insert because of CatalogName->Catalog PK", (done)=>{
     db.exec(`INSERT INTO CatalogName (CatalogID, Name, DateModified) 
         VALUES (
             'cat5', 'My cat1 name', datetime('now')
-        )`, (err)=>{
+        );`, (err)=>{
+            db.exec(`DELETE FROM CatalogName;`)
             if(err){
                 return done()
             }
@@ -25,13 +52,74 @@ it("inserts into CatalogName", (done)=>{
     db.exec(`INSERT INTO CatalogName (CatalogID, Name, DateModified) 
         VALUES (
             'cat1', 'My cat1 name', datetime('now')
-        )`, (err, result)=>{
+        );`, (err)=>{
+            db.exec(`DELETE FROM CatalogName;`)
             if(err){
                 throw new Error("Should have worked")
             }
             return done()
         })
 })
+
+
+it("Fails insert because of CatalogDeterminationLookup->CatalogDetermination FK", (done)=>{
+    db.exec(`INSERT INTO CatalogDetermination (CatalogID, DeterminationType, DateModified) 
+    VALUES (
+        'cat1', 'noexist', datetime('now')
+    );`, (err)=>{
+        db.exec(`DELETE FROM CatalogDetermination;`)
+        if(err){
+            return done()
+        }
+        throw new Error("Should have errored")
+        done()
+    })      
+    
+})
+it("inserts records into CatalogDetermination", (done)=>{
+    db.exec(`INSERT INTO CatalogDetermination (CatalogID, DeterminationType, DateModified) 
+    VALUES (
+        'cat1', 'Model', datetime('now')
+    );`, (err)=>{
+        db.exec(`DELETE FROM CatalogDetermination;`)
+        if(err){
+            throw new Error("Should not have errored")
+        }
+        return done()
+    })
+    
+})
+
+
+it("Fails insert because of CreateCatalogEventLookup->CatalogEvents FK", (done)=>{
+    db.exec(`INSERT INTO CatalogEvents (CatalogID, CatalogEvent, DateOfEvent, DateModified) 
+    VALUES (
+        'cat1', 'noexist',date('2016-01-01'), datetime('now')
+    );`, (err)=>{
+        db.exec(`DELETE FROM CatalogEvents;`)
+        if(err){
+            return done()
+        }
+        throw new Error("Should have errored")
+        done()
+    })      
+    
+})
+it("inserts records into CatalogEvents", (done)=>{
+    db.exec(`INSERT INTO CatalogEvents (CatalogID, CatalogEvent, DateOfEvent, DateModified) 
+    VALUES (
+        'cat1', 'Implementation Date', date('2016-01-01'), datetime('now')
+    );`, (err)=>{
+        db.exec(`DELETE FROM CatalogEvents;`)
+        if(err){
+            throw new Error("Should not have errored")
+        }
+        return done()
+    })
+    
+})
+
+
 
 
 
